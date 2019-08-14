3808a90784b4da2e3f9e978e8cf9a65e0fa5687f
cloedy <skywangqj@gmail.com>
2019-08-14 16:17:15 +0800

    fix initial files

--- app/TestCase.php
+++ app/TestCase.php
@@ -9,5 +9,10 @@ namespace C;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-
+    // demo use 
+    // public function testDo()
+    // {
+    //     c()->get('http://www.baidu.com');
+    //     c()->post('http://www.baidu.com');
+    // }
 }
