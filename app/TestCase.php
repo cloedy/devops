2c6f5e02f41c1c6be9d3d8bc1d8fcae0edcb6cd1
cloedy <skywangqj@gmail.com>
2019-08-16 15:18:36 +0800

    Update TestCase.php

--- app/TestCase.php
+++ app/TestCase.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by wangqianjun.
- * Mail: wangqianjun@chehang168.com
- * Date: 2019/8/14
- * Time: 15:23
- */
+
 namespace C;
 
 class TestCase extends \PHPUnit\Framework\TestCase
